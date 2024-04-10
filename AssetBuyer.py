19a4903f68f655387af68f637490d0507ddcad2e
Roblox-Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-04-10 10:14:25 -0400

    a

--- AssetBuyer.py
+++ AssetBuyer.py
@@ -7,10 +7,9 @@ class Buyer:
         self.session.cookies.set(".ROBLOSECURITY", cookie, domain=".roblox.com")
         self.session.headers
 
-        # Debuging
+        # Debugging
         # req = self.session.get("https://users.roblox.com/v1/users/authenticated").text
         # print(req)
     
     def get_csrf(self) -> str:
         return self.session.post('https://auth.roblox.com/v1/login').headers['x-csrf-token']
-
